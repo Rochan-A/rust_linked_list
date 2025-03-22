358b85732e11be623abf3ef399ef486cfc1a551d
Rochan Avlur <rochan.avlur@gmail.com>
2025-03-22 16:01:46 -0700

    doubly linked list - *_back versions

--- src/doubly_linked_deque.rs
+++ src/doubly_linked_deque.rs
@@ -5,7 +5,7 @@
  - fn borrow_mut(&self) -> RefMut<'_, T>    (like &mut, requires exclusivity)
 */
 
-use std::cell::RefCell;
+use std::cell::{Ref, RefCell, RefMut};
 use std::rc::Rc;
 
 pub struct List<T> {
@@ -85,6 +85,71 @@ impl<T> List<T> {
             Rc::try_unwrap(curr_head).ok().unwrap().into_inner().elem
         })
     }
+
+    /*
+    Ref and RefMut implement Deref and DerefMut respectively. So for most intents and purposes they
+    behave exactly like &T and &mut T. However, because of how those traits work, the reference
+    that's returned is connected to the lifetime of the Ref, and not the actual RefCell. This means
+    that the Ref has to be sitting around as long as we keep the reference around.
+     */
+    pub fn peek_front(&self) -> Option<Ref<T>> {
+        self.head
+            .as_ref()
+            /* just like you can map over an Option, you can map over a Ref. */
+            .map(|node| Ref::map(node.borrow(), |node| &node.elem))
+    }
+
+    pub fn peek_front_mut(&mut self) -> Option<RefMut<T>> {
+        self.head
+            .as_ref()
+            .map(|node| RefMut::map(node.borrow_mut(), |node| &mut node.elem))
+    }
+
+    /* --------------- *_back versions of the above methods ----------------- */
+
+    pub fn push_back(&mut self, elem: T) {
+        let new_tail = Node::new(elem);
+
+        match self.tail.take() {
+            Some(curr_tail) => {
+                curr_tail.borrow_mut().next = Some(new_tail.clone());
+                new_tail.borrow_mut().prev = Some(curr_tail);
+                self.tail = Some(new_tail);
+            }
+            None => {
+                self.head = Some(new_tail.clone());
+                // order matters here.
+                self.tail = Some(new_tail);
+            }
+        }
+    }
+
+    pub fn pop_back(&mut self) -> Option<T> {
+        self.tail.take().map(|curr_tail| {
+            match curr_tail.borrow_mut().prev.take() {
+                Some(new_tail) => {
+                    new_tail.borrow_mut().next.take();
+                    self.tail = Some(new_tail);
+                }
+                None => {
+                    self.head.take();
+                }
+            }
+            Rc::try_unwrap(curr_tail).ok().unwrap().into_inner().elem
+        })
+    }
+
+    pub fn peek_back(&self) -> Option<Ref<T>> {
+        self.tail
+            .as_ref()
+            .map(|node| Ref::map(node.borrow(), |node| &node.elem))
+    }
+
+    pub fn peek_back_mut(&mut self) -> Option<RefMut<T>> {
+        self.tail
+            .as_ref()
+            .map(|node| RefMut::map(node.borrow_mut(), |node| &mut node.elem))
+    }
 }
 
 // Destructor.
@@ -126,5 +191,49 @@ mod test {
         // Check exhaustion
         assert_eq!(list.pop_front(), Some(1));
         assert_eq!(list.pop_front(), None);
+
+        // ---- back -----
+
+        // Check empty list behaves right
+        assert_eq!(list.pop_back(), None);
+
+        // Populate list
+        list.push_back(1);
+        list.push_back(2);
+        list.push_back(3);
+
+        // Check normal removal
+        assert_eq!(list.pop_back(), Some(3));
+        assert_eq!(list.pop_back(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push_back(4);
+        list.push_back(5);
+
+        // Check normal removal
+        assert_eq!(list.pop_back(), Some(5));
+        assert_eq!(list.pop_back(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop_back(), Some(1));
+        assert_eq!(list.pop_back(), None);
+    }
+
+    #[test]
+    fn peek() {
+        let mut list = List::new();
+        assert!(list.peek_front().is_none());
+        assert!(list.peek_back().is_none());
+        assert!(list.peek_front_mut().is_none());
+        assert!(list.peek_back_mut().is_none());
+
+        list.push_front(1);
+        list.push_front(2);
+        list.push_front(3);
+
+        assert_eq!(&*list.peek_front().unwrap(), &3);
+        assert_eq!(&mut *list.peek_front_mut().unwrap(), &mut 3);
+        assert_eq!(&*list.peek_back().unwrap(), &1);
+        assert_eq!(&mut *list.peek_back_mut().unwrap(), &mut 1);
     }
 }
